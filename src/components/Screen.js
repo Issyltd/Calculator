a0790631cb7e9fce984838c66e58301575ab7bf0
Isma'il M <145236107+Smiley-Ldn@users.noreply.github.com>
2023-09-24 21:56:15 +0100

    Add files via upload

---
+++ src/components/Screen.js
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
+
+const Screen = () => {
+  const { calc } = useContext(CalcContext);
+
+  return (
+    <div className="screen">
+      <span className="text">{calc.num ? calc.num : calc.res}</span>
+    </div>
+  );
+};
+
+export default Screen;
